cb5520f845c570446b8e2342929aca7da1cb9292
shitheesh <shitheeshakash@gmail.com>
2021-09-10 20:55:41 +0530

    Dark mode added

--- sillypoint/assets/js/core.js
+++ sillypoint/assets/js/core.js
@@ -4,21 +4,20 @@ $(document).ready(function() {
     $('.image-container').on('click', function() {
         i = i - 1;
         i = (i < 1) ? 0 : i;
-        let css = `transform : translateY(${ i * -1 * height }px);`;
+        let css = `transform : translateY(${i * -1 * height}px);`;
         $('#home').attr('style', css);
     })
     $('.post-wrapper').on('click', function() {
         i = i + 1;
         i = (i > 3) ? 0 : i;
-        let css = `transform : translateY(${ i * -1 * height }px);`;
+        let css = `transform : translateY(${i * -1 * height}px);`;
         $('#home').attr('style', css);
     })
     $('.header').click(function() {
         $('#home').toggle();
         $('#menu').toggle();
-        // if($('#home').is(':visible')){
-        //     $('#home').hide();
-        //     $
-        // }
+    })
+    $('.darkmode').change(function() {
+        (this.checked) ? $('body').addClass('theme-dark'): $('body').removeClass('theme-dark');
     })
 });
