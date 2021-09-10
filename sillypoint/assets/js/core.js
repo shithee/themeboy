78874d66376fd497bd664f641f8e0010335af254
shitheesh <shitheeshakash@gmail.com>
2021-09-10 15:38:59 +0530

    Swiping functionality -- done

---
+++ sillypoint/assets/js/core.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    var i = 0;
+    var height = (window.innerHeight - 50);
+    $('.image-container').on('click', function() {
+        i = i - 1;
+        i = (i < 1) ? 0 : i;
+        let css = `transform : translateY(${ i * -1 * height }px);`;
+        $('#home').attr('style', css);
+    })
+    $('.post-wrapper').on('click', function() {
+        i = i + 1;
+        i = (i > 3) ? 0 : i;
+        let css = `transform : translateY(${ i * -1 * height }px);`;
+        $('#home').attr('style', css);
+    })
+});
