8be1d223580eba045a13829bf6234976f2a878f7
shitheesh <shitheeshakash@gmail.com>
2022-09-09 15:00:24 +0530

    Cricktist RSS site -- phase 1

--- cricketist/assets/js/core.js
+++ cricketist/assets/js/core.js
@@ -19,6 +19,33 @@ $(document).ready(function() {
     })
     $('.darkmode').change(function() {
         (this.checked) ? $('body').addClass('theme-dark'): $('body').removeClass('theme-dark');
-    })
+    });
+    $('.btn1').click(function(){
+        let cont = $('.maincontainer');
+        let el = cont.find('section.active');
+        el = (!el.length || el.hasClass('footer')) ? cont.find('section').first() : el.next(); 
+        cont.find('section').not('.footer').hide().removeClass('active');
+        el.addClass('active').show();
+    });
+    $('.btn3').click(function(){
+        let el = $('body');
+        el.removeClass('themedark');
+        $(this).hide().siblings('.btn2').show();
+    });
+    $('.btn2').click(function(){
+        let el = $('body');
+        el.addClass('themedark');
+        $(this).hide().siblings('.btn3').show();
+    });
+    $('.btn4').click(function(){
+        let el = $('body');
+        el.removeClass('themegrid');
+        $(this).hide().siblings('.btn5').show();
+    });
+    $('.btn5').click(function(){
+        let el = $('body');
+        el.addClass('themegrid');
+        $(this).hide().siblings('.btn4').show();
+    });
 });
 // Preloaders.net for gif
